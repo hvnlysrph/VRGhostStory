f6a61093b09916b560a0a182e0eda0936960e690
Bridget Cloud <bridgetcloud@ymail.com>
2020-03-26 14:08:36 -0500

    SummonOrb
    Orb is now summoned to player when the candlestick is picked up.  Orb goes back to hovering in front of player when the candle stick is set down.
    -Need to fix colider on candle stick so it will adjust if it has landed upside down.

---
+++ VRGhostStory/Assets/MyScripts/OrbBehavior.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using OVRTouchSample;
+using VRTK.Prefabs.Interactions.Interactors;
+
+public class OrbBehavior : MonoBehaviour
+{
+    public GameObject movePoint;
+    public Transform orbContainer;
+    public GameObject playerPos;
+    public GameObject ghostOrb;
+    public GameObject[] followPts;
+    
+
+    Vector3 movementDir;
+
+
+    const float movementDistance = 0.1f;
+    float movementSpeed = 1f;
+    private bool anchorCamera = true, anchorHand = false;
+    float dist;
+    int spwnPt = 0;
+
+    public bool AnchorCamera
+    {
+        set
+        {
+            anchorCamera = value;
+        }
+    }
+
+    /************************************************/
+    private void Awake()
+    {
+        //PlayerInput.onTriggerDown += GoToHand;
+
+    }
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (anchorCamera)
+        {
+            FollowPlayer();
+        }
+        else if (anchorHand)
+        {
+            GoToHand();
+        }
+
+        //Invoke("LeadPlayer", 10f);
+
+        dist = Vector3.Distance(playerPos.transform.position, transform.position);
+        
+
+    }
+
+    private void FollowPlayer()
+    {
+        movementDir = (orbContainer.position - transform.position).normalized;
+
+        if((transform.position-orbContainer.position).magnitude > movementDistance)
+        {
+            transform.Translate(movementDir * Time.deltaTime * movementSpeed);
+        }
+        
+    }
+
+    private void LeadPlayer()
+    {
+        anchorCamera = false;
+
+        if (spwnPt < followPts.Length)
+        {
+            bool canMove = true;
+            GameObject transportPt = followPts[spwnPt].transform.GetChild(0).gameObject;
+            movementDir = (followPts[spwnPt].transform.position - transform.position).normalized;
+
+            transportPt.SetActive(true);
+
+            if ((transform.position - followPts[spwnPt].transform.position).magnitude > 0.5f && canMove)
+            {
+                transform.Translate(movementDir * Time.deltaTime * 3f);
+
+            }
+            else
+            {
+                canMove = false;
+
+                if (dist < 2.5)
+                {
+                    transportPt.SetActive(false);
+                    spwnPt++;
+                    LeadPlayer();
+                }
+            }
+
+        }
+        else
+        {
+            anchorCamera = true;
+        }
+    }
+
+ 
+    void GoToHand()
+    {
+        Debug.Log("go to hand");
+        anchorCamera = false;
+
+        movementDir = (movePoint.transform.position - transform.position).normalized;
+
+        if ((transform.position - movePoint.transform.position).magnitude > .01)
+        {
+            transform.Translate(movementDir * Time.deltaTime * movementSpeed);
+        }
+
+    }
+
+    public void Summoned()
+    {
+        anchorCamera = false;
+        anchorHand = true;
+    }
+
+    public void Released()
+    {
+        anchorHand = false;
+        anchorCamera = true;
+    }
+
+}
---
+++ VRGhostStory/Assets/MyScripts/PickedPosition.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickedPosition : MonoBehaviour
+{
+    public void PickedUp()
+    {
+
+        transform.localPosition = new Vector3(-0.18f ,1f,0f);
+        transform.localRotation = Quaternion.Euler(-71.53f, 4f, -18.8f);
+    }
+
+    public void Dropped()
+    {
+        transform.localPosition = new Vector3(0,0,0);
+
+        transform.localRotation = Quaternion.Euler(-90f,0,0);
+    }
+}
